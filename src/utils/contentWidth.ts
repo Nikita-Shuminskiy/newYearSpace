5baa3073a9ed358ddcef98f04c21409bdf31d28c
Nick Shuminskiy <shuminskiy23@gmail.com>
2025-12-07 20:02:20 +0300

    refactor(Competition): simplify content width logic, introduce CheckmarkIcon component, and update styling for improved responsiveness

---
+++ src/utils/contentWidth.ts
@@ -0,0 +1,14 @@
+export type ContentWidth = "desktop-xl-only" | "desktop-only" | "tablet-only" | "mobile-only";
+
+export const getContentWidth = (width: number): ContentWidth => {
+    if (width >= 1980) {
+        return 'desktop-xl-only';
+    } else if (width <= 1979 && width >= 1280) {
+        return 'desktop-only';
+    } else if (width < 1280 && width >= 744) {
+        return 'tablet-only';
+    } else {
+        return 'mobile-only';
+    }
+};
+
